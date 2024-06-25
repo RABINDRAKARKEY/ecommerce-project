f1f8ad7757e111e63337eb31fc2a260b85a553de
RABINDRAKARKEY <108052235+RABINDRAKARKEY@users.noreply.github.com>
2024-06-25 09:18:53 +0545

    commit

---
+++ AsimpleWork/index.js
@@ -0,0 +1,57 @@
+// console.log("HEllo World please!!!")
+
+// variables
+// var x = 30;
+// console.log(x);
+// var x = 100;
+// console.log(x);
+// ______________________________________________________________________________________________________________________
+// const
+// var x = "const";
+// console.log(text)
+// const result = 90;
+// console.log(result)
+// ______________________________________________________________________________________________________________________
+// String 
+// result = 100; // error
+// console.log(result);
+
+// datatypes in javascript:
+// const output = "Bipin Karki"
+// console.log(output)
+
+// Number 
+// const result = 90;
+// console.log(result);
+// console.log(typeof result);
+
+// float
+// const x = 3.56;
+// console.log(x);
+
+// undefined:
+// let y;
+// console.log(y);
+// console.log(typeof y);
+
+
+// Null
+// let y = null;
+// console.log(y);
+// console.log(typeof y);
+
+// boolean:
+// const isTrue = true;
+// console.log(isTrue);
+// console.log(typeof isTrue);
+
+// bigint
+// const result = 46546465541n;
+// console.log(typeof result);
+// console.log(result);
+
+
+// symbol
+const output =  Symbol("Hello world!!!");
+console.log(output);
+console.log(typeof output);
